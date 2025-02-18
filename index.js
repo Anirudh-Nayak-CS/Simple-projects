818d0d600dd621f10032c2aecb6efb903a7c4d05
Anirudh-Nayak-CS <anirudhnayak1508@gmail.com>
2025-02-18 20:39:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+/*let username;
+document.getElementById("Mysubmit").onclick=function() {
+username=document.getElementById("Mytext").value;
+document.getElementById("Title").textContent=`My name is ${username}`;
+}
+
+let age;
+age=window.prompt(`What is your age?`);
+age=Number(age);
+age+=1;
+console.log(age);
+
+let x='pizza';
+let y;
+x=Number(x);
+
+console.log(x,typeof(x))
+
+
+const PI=3.14159
+let radius;
+let circumference;
+PI=3
+radius=window.prompt(`What is the radius`);
+radius=Number(radius);
+circumference=2*PI*radius
+console.log(circumference)
+
+const PI=3.1415;
+let radius;
+let circumference;
+document.getElementById('hit').onclick=function() {
+radius=document.getElementById('radiusientered').value
+circumference=2*PI*radius;
+document.getElementById('Answer').textContent=`The circumference of the circle is ${circumference}`;
+}*/
+
